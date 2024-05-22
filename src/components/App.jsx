a938b4ab85f161e0f1a6cb94451ad739887aceec
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-05-22 22:27:56 +0200

    first jsx template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,35 @@ function App() {
 
   return (
     <>
-        <p>evaluacion 3</p>
+        <header>
+            Logo Rick and Morty
+        </header>
+
+        <section>
+            Filters
+        </section>
+
+        <section>
+            {/* https://rickandmortyapi.com/api/character */}
+            CharacterList
+
+            <article>
+                <div>Picture1</div>
+                <div>Name1</div>
+                <div>Specie1</div>
+            </article>
+
+            <article>
+                <div>Picture2</div>
+                <div>Name2</div>
+                <div>Specie2</div>
+            </article>
+
+        </section>
+
+        <section>
+            CharacterDetail
+        </section>
     </>
   )
 }

6385215dc55ff83910f300c16f7dcb0031b6905e
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-05-22 23:43:24 +0200

    adss sass

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,16 +1,18 @@
+import '../styles/CharaterList.css';
+
 function CharacterList() {
     return (
         <section>
             {/* https://rickandmortyapi.com/api/character */}
             CharacterList
 
-            <article>
+            <article className='characterCard'>
                 <div>Picture1</div>
                 <div>Name1</div>
                 <div>Specie1</div>
             </article>
 
-            <article>
+            <article className="characterCard">
                 <div>Picture2</div>
                 <div>Name2</div>
                 <div>Specie2</div>

376a1aaec5a2e531b02346db4b7470357edbb190
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-05-23 06:49:59 +0200

    adds form and styles to filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -4,7 +4,9 @@ function Filters() {
 
     return (
         <section className='filtersSection'>
-            Filters
+            <form className='filtersSection__searchCharacter'>
+                <input type='text' id='searchCharacterInput' className='filtersSection__searchCharacter--input'/>
+            </form>
         </section>
     );
 }

833573e39ff7292ba373934c1c54c22696bf3dc0
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-05-23 10:17:30 +0200

    improves CharacterCard style

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -6,8 +6,8 @@ function CharacterCard() {
         <article className='characterCard'>
             <img src={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}  />
             <div className='characterCard__description'>
-                <div>Rick Sanchez</div>
-                <div>Human</div>
+                <div className='characterCard__description__name'>Rick Sanchez</div>
+                <div className='characterCard__description__specie'>Human</div>
             </div>
         </article>
     );
